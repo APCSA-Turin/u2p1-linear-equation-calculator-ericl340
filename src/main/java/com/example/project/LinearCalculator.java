81047b6045aab02a1c9bd929c2332e8d5525fc6c
ericl340 <ericl340@nycstudents.net>
2024-10-17 13:32:21 +0000

    u

--- src/main/java/com/example/project/LinearCalculator.java
+++ src/main/java/com/example/project/LinearCalculator.java
@@ -14,10 +14,10 @@ public class LinearCalculator{
     //For example, "(1,2)" and "(3,4)" would be two parameter values 
     //You will have to parse the string into 4 integers, representing the 2 points.
     public LinearCalculator(String c1, String c2){ // <--add 2 string parameters to this constructor
-        x1 = Integer.parseInt(c1.split(",")[0].substring(1));
-        y1 = Integer.parseInt(c1.substring(0, c1.length() - 1).split(",")[1]);
-        x2 = Integer.parseInt(c2.split(",")[0].substring(1));
-        y2 = Integer.parseInt(c1.substring(0, c1.length() - 1).split(",")[1]);
+        x1 = Integer.parseInt(c1.split(",")[0].substring(1));//splits the coords by the comma into an array and takes the 0th index, and removes the first char (
+        y1 = Integer.parseInt(c1.substring(0, c1.length() - 1).split(",")[1]);//removes the last char ) and then splits array at the comma, then gets index 1
+        x2 = Integer.parseInt(c2.split(",")[0].substring(1));//does the same thing as the prev for the second set of coords
+        y2 = Integer.parseInt(c2.substring(0, c2.length() - 1).split(",")[1]);
     }
 
 
@@ -37,20 +37,24 @@ public class LinearCalculator{
     //distance() -> returns a double. 
     //calculates the distance between the two points to the nearest HUNDREDTH and returns the value.
     public double distance(){
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return roundedToHundredth(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));//uses a^2 + b^2 = c^2
     }
     //yInt() -> returns a double.
     //calculates the y intercept of the equation and returns the value to the nearest HUNDREDTH
     //if y-int if undefined, should return -999.99
     public double yInt(){
-        return y1 == y2 ? -999.99 : y1 - slope() * x1;
+        return x1 == x2 ? -999.99 : roundedToHundredth(y1 - slope() * x1);
+        //if the x vars are equal, the slope would be undefined, meaning no yint
+        //if the x vars unequal, returns the yint using b = y - mx rounded
     }
 
     //slope() -> returns a double. 
     //calculates the slope of the equations and returns the value to the nearest HUNDREDTH
     //if slope is undefined, should return -999.99
     public double slope(){
-        return y1 == y2 ? -999.99 : (y2 - y1) / (x2 - x1);
+        return x1 == x2 ? -999.99 : roundedToHundredth((double)(y2 - y1) / (x2 - x1));
+        //if the x vars are equal, the slope would be undefined
+        //if the y vars are unequal, returns delta y / delta x rounded
     }
 
     //equations() -> returns a String.
@@ -58,14 +62,20 @@ public class LinearCalculator{
     //if the equation has no slope, the equation should return -> "undefined"
     //HINT: You may need other custom methods to decrease the amount of code in the equations() method
     public String equation(){
-        return slope() == -999.99 ? null : "y=" + slope() + "x+" + yInt();
+        return slope() == -999.99 ? "undefined" : "y=" + (slope() == 0 ? "" : slope() + "x") + (slope() == 0 || yInt() <= 0 ? "" : "+") + (yInt() == 0 ? "" : yInt());
+        //returns undefined if the slope is undefined
+        //returns y=mx+b otherwise
+        //if x is 0, mx is removed
+        //if b is 0, its removed
+        //the + in between is removed when b is 0 or negative
     }
 
 
     //roundedToHundredth(double x)-> returns double
     //calculates the input to the nearest hundredth and returns that value
     public double roundedToHundredth(double x){
-        return Math.round(x * 100) / 100;
+        return Math.round(x * 100) / 100.0;
+        //multiplies by 100, rounds, then devides by 100 to round to the nearest hundredth
     }
 
     //printInfo() -> returns a string of information
--- src/main/java/com/example/project/a.java
+++ src/main/java/com/example/project/a.java
@@ -2,7 +2,7 @@ package com.example.project;
 
 public class a {
     public static void main(String[] args) {
-        LinearCalculator c = new LinearCalculator("(0,1)", "(2,3)");
-        System.out.println(c.getY2());
+        LinearCalculator c = new LinearCalculator("(10,5)","(6,-1)");
+        System.out.println(c.printInfo());
     }
 }
